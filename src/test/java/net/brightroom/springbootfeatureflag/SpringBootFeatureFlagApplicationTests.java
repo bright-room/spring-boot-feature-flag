d8376148f2214539e0ff84a704bb757818e3240b
kukv <rio.by.kukv@gmail.com>
2025-02-15 14:37:12 +0900

    initial commit

---
+++ src/test/java/net/brightroom/springbootfeatureflag/SpringBootFeatureFlagApplicationTests.java
@@ -0,0 +1,13 @@
+package net.brightroom.springbootfeatureflag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeatureFlagApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
