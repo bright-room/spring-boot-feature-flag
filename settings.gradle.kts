33b5b3c984721bbef80cf2bae65ae135ae217a11
kukv <rio.by.kukv@gmail.com>
2025-02-15 14:53:05 +0900

    Make the project a multi-module configuration.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "spring-boot-feature-flag"
